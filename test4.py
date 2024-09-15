4da82d98f21db3a7ba94cf4bfdf828ebd3c0771c
rayssams0001 <paulostand17@gmail.com>
2024-09-15 13:18:44 -0300

    Add files via upload

---
+++ test4.py
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset='utf-8'>
+        <meta name="rayssa">
+        <content='width=device-width,initial-scale=100'>
+            <title>helo word</title>
+    </head>
+    <body>
+        <h1></h1>
+    </body>
+    </html>
